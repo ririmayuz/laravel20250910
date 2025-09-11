6841339dc3d6ee5d4b6da30630ec1d3ceb54b330
Yuz <wwowkdrr@gmail.com>
2025-09-11 15:10:03 +0800

    修改index的62行 改回正確的程式碼

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -59,10 +59,11 @@
                         <td>{{$value->id}}</td>
                         <td>{{$value->name}}</td>
                         <td>
-                            <a class="btn btn-success" href="{{route('students.edit',['student' => $value['id']])}}">修改</a>
+                            <a class="btn btn-success" href="{{route('students.edit',['student' => $value->id])}}">修改</a>
                         </td>
                     </tr>
                 @endforeach
+                
 
                 {{-- 原生php --}}
                 <?php foreach ($data as $key => $value) :?>
